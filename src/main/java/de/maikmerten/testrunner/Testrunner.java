788213e4fb0b338fd03326c20c703eca3db1a6ac
merten <maikmerten@googlemail.com>
2014-02-09 21:57:36 +0100

    move output for logs to LogWriter, also generate a SSIM graph with logarithmic bitrate axis

--- src/main/java/de/maikmerten/testrunner/Testrunner.java
+++ src/main/java/de/maikmerten/testrunner/Testrunner.java
@@ -2,30 +2,16 @@ package de.maikmerten.testrunner;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.NumberTickUnit;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.renderer.xy.XYSplineRenderer;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  *
@@ -126,117 +112,6 @@ public class Testrunner {
         }
     }
     
-    private void writeGlobalLog() throws Exception {
-        List<LogEntry> allLogs = new ArrayList<>();
-        for(String key : logs.keySet()) {
-            allLogs.addAll(logs.get(key));
-        }
-        Collections.sort(allLogs, LogEntry.getComparator());
-        
-        Set<File> globallogfiles = new HashSet();
-        for(LogEntry entry : allLogs) {
-            globallogfiles.add(entry.test.globallogfile);
-        }
-        
-        for(File logfile : globallogfiles) {
-            if(logfile.exists()) {
-                logfile.delete();
-            }
-            
-            // assemble all entries for this log file
-            List<LogEntry> entries = new ArrayList<>();
-            Set<String> codecset = new HashSet<>();
-            for(LogEntry e : allLogs) {
-                if(e.test.globallogfile.equals(logfile)) {
-                    entries.add(e);
-                    codecset.add(e.test.codecname);
-                }
-            }
-            List<String> codecs = new ArrayList<>(codecset);
-            Collections.sort(codecs);
-            
-            StringBuilder sb = new StringBuilder();
-            
-            // build header
-            sb.append("Bitrate (kbps);");
-            for(String codec : codecs) {
-                sb.append(codec).append(";");
-            }
-            sb.append("\n");
-            
-            // write entries
-            for(LogEntry entry : entries) {
-                sb.append(entry.bitrate);
-                int idx = codecs.indexOf(entry.test.codecname);
-                // push entry to correct column
-                for(int i = 0; i < idx; ++i) {
-                    sb.append(";");
-                }
-                sb.append(";").append(entry.ssimScore);
-                int othercols = codecs.size() - 1 - idx;
-                for(int i = 0; i < othercols; ++i) {
-                    sb.append(";");
-                }
-                sb.append("\n");
-            }
-            
-            FileOutputStream fos = new FileOutputStream(logfile);
-            fos.write(sb.toString().getBytes());
-            fos.close();
-
-            // generate chart with JFreeChart
-            String inputname = "";
-            double maxssim = 0;
-            double minssim = 999999;
-            int tickunit = 64;
-            File chartfile = new File(logfile.getAbsolutePath() + ".png");
-            if(chartfile.exists()) {
-                chartfile.delete();
-            }
-            
-            XYSeriesCollection collection = new XYSeriesCollection();
-            Map<String, XYSeries> seriesmap = new HashMap<>();
-            for(LogEntry entry : entries) {
-                String codecname = entry.test.codecname;
-                XYSeries series = seriesmap.get(codecname);
-                if(series == null) {
-                    series = new XYSeries(codecname);
-                    seriesmap.put(codecname, series);
-                    collection.addSeries(series);
-                }
-                series.add(entry.bitrate, entry.ssimScore);
-                
-                if(entry.ssimScore < minssim) {
-                    minssim = entry.ssimScore;
-                }
-                if(entry.ssimScore > maxssim) {
-                    maxssim = entry.ssimScore;
-                }
-                tickunit = entry.test.kbitinc;
-                inputname = entry.test.input.getName();
-            }
-            
-            final JFreeChart chart = ChartFactory.createXYLineChart(
-                    inputname,
-                    "Bitrate (kbps)",
-                    "SSIM",
-                    collection,
-                    PlotOrientation.VERTICAL,
-                    true,
-                    true,
-                    false
-            );
-            // set some chart render options and save to PNG
-            chart.getXYPlot().setRenderer(new XYSplineRenderer());
-            chart.getXYPlot().getRangeAxis().setRange(minssim - 0.5, maxssim + 0.5);
-            NumberAxis xAxis = (NumberAxis) chart.getXYPlot().getDomainAxis();
-            xAxis.setTickUnit(new NumberTickUnit(tickunit));
-            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) chart.getXYPlot().getRenderer();
-            renderer.setBaseShapesVisible(true);
-            ChartUtilities.saveChartAsPNG(chartfile, chart, 800, 600);
-        }
-    }
-    
 
     private void runTest(TestDescription test) throws Exception {
         int kbits = test.kbitstart;
@@ -330,21 +205,8 @@ public class Testrunner {
         }
         
         // write logs
-        for(String logfile : logs.keySet()) {
-            List<LogEntry> log = logs.get(logfile);
-            Collections.sort(log, LogEntry.getComparator());
-            File f = new File(logfile);
-            if(f.exists()) {
-                f.delete();
-            }
-            try (FileOutputStream logstream = new FileOutputStream(f)) {
-                logstream.write(LogEntry.getCsvHeader().getBytes());
-                for(LogEntry entry : log) {
-                    logstream.write(entry.getCsvString().getBytes());
-                }
-            }
-        }
-        writeGlobalLog();
+        LogWriter.writeEncoderLogs(logs);
+        LogWriter.writeGlobalLog(logs);
     }
     
 
